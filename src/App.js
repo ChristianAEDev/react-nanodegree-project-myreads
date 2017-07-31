2d994c10e23da32512e52b70da54d00c3d8db6e9
Christian <ChristianAE.DEV@gmail.com>
2017-07-31 16:03:48 +0100

    Work on implementation to add new book

--- src/App.js
+++ src/App.js
@@ -16,16 +16,9 @@ class BooksApp extends React.Component {
 
     this.state = {
       books: [
-        { id: "1", title: "To Kill a Mockingbird", author: "Harper Lee", shelf: SHELF_CURRENTLY_READING, imageLinks: { smallThumbnail: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api" } },
-        { id: "2", title: "Ender's Game", author: "Orson Scott Card", shelf: SHELF_CURRENTLY_READING, imageLinks: { smallThumbnail: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api" } },
-        { id: "3", title: "1776", author: "David McCullough", shelf: SHELF_WANT_TO_READ, imageLinks: { smallThumbnail: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api" } },
-        { id: "4", title: "Harry Potter and the Sorcerer's Stone", author: "J.K. Rowling", shelf: SHELF_WANT_TO_READ, imageLinks: { smallThumbnail: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api" } },
-        { id: "5", title: "The Hobbit", author: "J.R.R. Tolkien", shelf: SHELF_READ, imageLinks: { smallThumbnail: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api" } },
-        { id: "6", title: "Oh, the Places You'll Go!", author: "Seuss", shelf: SHELF_READ, imageLinks: { smallThumbnail: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api" } },
-        { id: "7", title: "The Adventures of Tom Sawyer", author: "Mark Twain", shelf: SHELF_READ, imageLinks: { smallThumbnail: "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api" } }
+        { "title": "iOS Programming", "subtitle": "The Big Nerd Ranch Guide", "authors": ["Christian Keur", "Aaron Hillegass"], "publisher": "Pearson Technology Group", "publishedDate": "2016-12-05", "description": "Updated for Xcode 8, Swift 3, and iOS 10, iOS Programming: The Big Nerd Ranch Guide leads you through the essential concepts, tools, and techniques for developing iOS applications. After completing this book, you will have the know-how and the confidence you need to tackle iOS projects of your own. Based on Big Nerd Ranch's popular iOS training and its well-tested materials and methodology, this bestselling guide teaches iOS concepts and coding in tandem. The result is instruction that is relevant and useful. Throughout the book, the authors explain what's important and share their insights into the larger context of the iOS platform. You get a real understanding of how iOS development works, the many features that are available, and when and where to apply what you've learned.", "industryIdentifiers": [{ "type": "ISBN_13", "identifier": "9780134682365" }, { "type": "ISBN_10", "identifier": "013468236X" }], "readingModes": { "text": true, "image": true }, "pageCount": 416, "printType": "BOOK", "categories": ["Computers"], "maturityRating": "NOT_MATURE", "allowAnonLogging": true, "contentVersion": "1.3.2.0.preview.3", "panelizationSummary": { "containsEpubBubbles": false, "containsImageBubbles": false }, "imageLinks": { "smallThumbnail": "http://books.google.com/books/content?id=XRekDQAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api", "thumbnail": "http://books.google.com/books/content?id=XRekDQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" }, "language": "en", "previewLink": "http://books.google.com/books?id=XRekDQAAQBAJ&printsec=frontcover&dq=ios&hl=&cd=1&source=gbs_api", "infoLink": "https://play.google.com/store/books/details?id=XRekDQAAQBAJ&source=gbs_api", "canonicalVolumeLink": "https://market.android.com/details?id=book-XRekDQAAQBAJ", "id": "XRekDQAAQBAJ", "shelf": SHELF_CURRENTLY_READING }
       ]
     }
-
     this.onMoveBook = this.onMoveBook.bind(this);
   }
 
@@ -36,25 +29,31 @@ class BooksApp extends React.Component {
 
     // Find the index of the selected book
     let indexSelectedBook = this.state.books.findIndex(v => v.id === selectedBook.id);
-        console.log(indexSelectedBook)
-    //If the index of the book is "-1" it is a book which is currently in no shelf
-    if (indexSelectedBook === -1) {
-      console.log("New book")
+
+    //If the current shelf is "none" it is a new book from the search
+    if (selectedBook.shelf === "none") {
+      console.log("add book")
+      selectedBook.shelf = targetShelf
       this.setState(state => ({
-        books: state.books.concat(selectedBook)
+        books: state.books.concat([selectedBook])
       }))
-      console.log(this.state.books);
     }
-
-
     //If the new state is "none" we just want to remove it from the books array
     else if (targetShelf === SHELF_NONE) {
+      console.log("remove book")
+      console.log("selectedBook.id: " + selectedBook.id)
       this.setState(state => ({
-        books: state.books.filter(book => book.id !== selectedBook.id)
+        books: state.books.filter(book => {
+          console.log("book.id: " + book.id)
+          console.log("return: " + book.id === selectedBook.id)
+          return book.id === selectedBook.id
+        })
       }))
+      console.log("after remove: " + JSON.stringify(this.state.books))
     }
     // Otherwise we move the book to the target shelf
     else {
+      console.log("move book")
       // Update the book in the shelf
       this.state.books[indexSelectedBook].shelf = targetShelf;
 
